1843ee7168082bd491c0417489fc2bcb0b1f7793
lizanagacueva <108812907+nagacuevaliza@users.noreply.github.com>
2022-10-16 18:48:10 +0300

    publishing lab3

---
+++ site3/elements/footer.php
@@ -0,0 +1,18 @@
+<!-- FOOTER -->
+<footer class="footer">
+      <div class="wrap">
+        <div class="footer_wrap">
+          <div>
+            <ul class="footer-info" id="footer-info">
+              <li class="footer-info_item" style="color: black">Нагацуева Елизавета Замировна</li>
+              <li class="footer-info_item" style="color: black">lizanagacueva@gmail.com</li>
+              <li class="footer-info_item" style="color: black">+7 (960) 429-30-77</li>
+              <li class="footer-info_item" style="color: black"><?= "Сформировано ", $today, " в ", $time?></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+   
+  </body>
+</html>
---
+++ site3/feedback_form.php
@@ -0,0 +1,12 @@
+<?php
+$title="Обратная связь";
+
+$today= date("m.d.y");
+$time = date("G:i:s");
+?>
+
+<?php require("elements/form_header.php")?>
+
+<?php require("elements/form_feedback.php")?>
+
+<?php require("elements/footer.php")?>
